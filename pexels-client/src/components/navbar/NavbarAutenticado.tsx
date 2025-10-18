997f30257d01552b754cf72f32e7f5c18d9d4823
MiguelRestrepoL <ith1zdragonz@gmail.com>
2025-10-18 02:53:28 -0500

    V2.1.6 (hotfix error deployment)

--- pexels-client/src/components/navbar/NavbarAutenticado.tsx
+++ pexels-client/src/components/navbar/NavbarAutenticado.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router";
 import { useAutenticacion } from "../../services/AutenticacionS";
-import "./navbar-autenticado.scss";
+import "./NavbarAutenticado.scss";
 
 /**
  * Barra de navegación para usuarios autenticados.
